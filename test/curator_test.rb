5f01c2c9c8272e19969434c54bd1cb88ecace66e
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-04 09:43:27 -0700

    Create test to test curator attributes with default values, initialize attributes, pass test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -14,4 +14,8 @@ class CuratorTest < Minitest::Test
     assert_instance_of Curator, @curator
   end
 
+  def test_it_has_attributes
+    assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
+  end
 end
