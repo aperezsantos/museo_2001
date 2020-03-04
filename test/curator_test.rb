a6dd3bfd83a8ff9063e56bb02ad226db5b23e7f7
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-04 09:53:09 -0700

    Add artist objects to setup, test artists can be added to collection

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,20 @@ class CuratorTest < Minitest::Test
       artist_id: "2",
       year: "1941"
       })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+      })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      })
   end
 
   def test_it_exists
@@ -36,5 +50,12 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
 
     assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_can_add_photographs
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
   end
 end
